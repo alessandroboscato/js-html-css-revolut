41d4a9d724c4d8cdcee0921162f862a3a00099ba
Alessandro <alessandro.boscato@gmail.com>
2020-09-03 18:17:14 +0200

    Hover testo black (bug)

--- js/script.js
+++ js/script.js
@@ -1,7 +1,18 @@
-$(".col-sm-8 li").hover(
-  function() {
-    $(this).find(".box_menu").removeClass("d-none")
-  }, function() {
-    $(this).find(".box_menu").addClass("d-none")
-  }
-);
+$(document).ready(function() {
+
+  $(".col-sm-8 li").hover(
+    function() {
+      $(this).find(".box_menu").removeClass("d-none")
+    }, function() {
+      $(this).find(".box_menu").addClass("d-none")
+    }
+  );
+
+  $("li").hover(
+    function() {
+      $(this).css("color", "black")
+    }, function() {
+      $(this).css("color", "#C3C7CA")
+    }
+  );
+});
